d1e939c529a4eb70c32b0e85a89ed5d7836c7727
Jacob M <moe.jacobw@gmail.com>
2022-03-02 14:04:05 -0600

    Added components
    Added in the Quote, Signup and Footer components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Components/Header';
 import Main from './Components/Main';
+import Quote from './Components/Quote';
+import SignUp from './Components/SignUp';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <main>
       <Header/>
       <Main />
+      <Quote />
+      <SignUp />
+      <Footer />
     </main>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark-blue text-white h-36 text-xl flex justify-center items-center">
+      <p>
+        Copyright &copy; The Odin Project 2022
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
