41cdec918f328f5c1b0f0a98f6cee41c24d8ec3f
Jacob M <moe.jacobw@gmail.com>
2022-03-31 07:14:25 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-dark-blue text-white h-36 text-xl flex justify-center items-center">
       <p>
-        Copyright &copy; The Odin Project 2022
+        Copyright &copy; The Odin Project 2022 (Jacob M edition)
       </p>
     </footer>
   )
